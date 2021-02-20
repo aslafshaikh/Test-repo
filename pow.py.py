0a5f01182fc3523fa35f043ac35ba0417d84119a
aslafshaikh <73557406+aslafshaikh@users.noreply.github.com>
2021-02-19 21:03:40 -0500

    Add files via upload

---
+++ pow.py.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("A simple program")
+base = int (input("Enter Base Value:"))
+power = int(input("Enter Power Value:"))
+print(f"{base}^{power}) = {base ** power}")
+
+
+# In[ ]:
+
+
+
+
